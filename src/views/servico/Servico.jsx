cb60bc2a812d6ca29a32c60530a57594eb951b25
unknown <drl1@discente.ifpe.br>
2023-05-02 20:52:43 -0300

    altração serviços

--- src/views/servico/Servico.jsx
+++ src/views/servico/Servico.jsx
@@ -1,18 +1,50 @@
 import React from "react";
-import { Grid } from 'semantic-ui-react';
+import { Link } from "react-router-dom";
+import { Button, Container, Divider, Grid, Icon } from 'semantic-ui-react';
 
 class Servico extends React.Component {
 
+
     render() {
         return (
+            
             <>
-                <Grid textAlign='center' 
-                style={{ height: '90vh' }} verticalAlign='middle'>
+                <Grid textAlign='center' style={{ height: '90vh' }} verticalAlign='middle'>
+                    <div style={{marginTop: '4%'}}>
+                        <Container textAlign="justified" >
+                            <h2> Petições </h2>
+                            <Divider/>
+                        
+                            <div style={{marginTop:'5%'}}>
+                                <Button animated>
+                                    <Button.Content visible>Consultar Petições</Button.Content>
+                                    <Button.Content hidden>
+                                    <Icon name="file alternate outline"/>
+                                    </Button.Content>
+                                    <Link to={'/consultarPeticao'}></Link>
+                                </Button>
+                                    
+                                <br/><br/><br/>
+                                <Button
+                                    type="button"
+                                    inverted
+                                    circular
+                                    icon
+                                    color='orange'
+                                    title='Consultar Petições'
+                                    >
+                                    <Icon name='clipboard outline' />
+                                    <Link to={'/criarPeticao'}> Criar Petição</Link>
+                                </Button>
 
-
-                    
+                            </div>
+                        </Container>
+                    </div>
                 </Grid>
+                
+               
             </>
+            
         )
     }
 }
