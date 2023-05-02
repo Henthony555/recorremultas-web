b2f458ed9891d73ddd96032d29cb9bdbf2af9da0
aluno <aluno@gmail.com>
2023-05-02 20:48:27 -0300

    02/05

--- src/views/usuario/Usuario.jsx
+++ src/views/usuario/Usuario.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Grid, Image } from 'semantic-ui-react';
+import { Container, Grid, Image, List } from 'semantic-ui-react';
 
 class Usuario extends React.Component{
 
@@ -7,9 +7,34 @@ class Usuario extends React.Component{
         return(
             <div>
                 <div style={{marginTop: '5%'}}>
-                    <Container>
-                        <h1>Usuário</h1>
-                    </Container>
+
+                <h2 class="ui header"><img src="/images/avatar/large/patrick.png" class="ui circular image"/> Usuario</h2>
+
+<List animated verticalAlign='middle'>
+    <List.Item>
+
+      <List.Content>
+        <List.Header> Dados Cadastados</List.Header>
+      </List.Content>
+    </List.Item>
+
+    <List.Item>
+
+      <List.Content>
+        <List.Header> Suas Petições </List.Header>
+      </List.Content>
+    </List.Item>
+
+    <List.Item>
+
+      <List.Content>
+        <List.Header> Alterar Senha </List.Header>
+      </List.Content>
+    </List.Item>
+
+  </List>
+
+
                 </div>
             </div>
         )
