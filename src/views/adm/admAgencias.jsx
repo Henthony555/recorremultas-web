e220a5a8c8461b93663e85b2bf0aac04278fc418
aluno <joao@gmail.com>
2023-05-05 20:25:47 -0300

    up agência

--- src/views/adm/admAgencias.jsx
+++ src/views/adm/admAgencias.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import InputMask from 'react-input-mask';
+import MapAgencias from "../../components/MapAgencias";
 import { Button, Form, Grid, Image, Container, Segment, Divider, Icon } from 'semantic-ui-react'
 
 class ADMagencias extends React.Component {
@@ -7,8 +9,45 @@ class ADMagencias extends React.Component {
     render() {
         return (
             <>
-                <Grid textAlign='center' verticalAlign='middle' style={{ height: '90vh' }} >
-                    <h1>(ADM)Agências</h1>
+                <Grid textAlign='center' verticalAlign='middle' style={{ height: '90vh' }}>
+                    <Grid.Column style={{ maxWidth: '90%' }}>
+                        <Grid columns={2}>
+                            <Grid.Column centered>
+
+                                <Form size='large' style={{ marginTop: '2%', maxWidth: 600 }}>
+                                    <h2>ADD Agência</h2>
+                                    <Form.Input
+                                        fluid
+                                        icon='user circle'
+                                        iconPosition='left'
+                                        placeholder='Nome do usuário'
+                                    />
+
+                                    <Form.Input
+                                        fluid
+                                        icon
+                                        iconPosition='left'
+                                    >
+                                        <Icon name='address card' />
+                                        <InputMask
+                                            placeholder='CPF'
+                                            mask="999.999.999-99"
+                                        />
+                                    </Form.Input>
+
+                                    <Button color='yellow' fluid size='large'>
+                                        Adicionar
+                                    </Button>
+
+                                </Form>
+                            </Grid.Column>
+
+                            <Grid.Column>
+                                <MapAgencias />
+                            </Grid.Column>
+                        </Grid>
+
+                    </Grid.Column>
                 </Grid>
             </>
         )
--- src/views/agencias/Agencias.jsx
+++ src/views/agencias/Agencias.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import MapAgencias from "../../components/MapAgencias";
-import { Grid } from 'semantic-ui-react';
+import { Grid, Segment, Container } from 'semantic-ui-react';
 
 class Agencias extends React.Component {
 
@@ -9,14 +9,21 @@ class Agencias extends React.Component {
             <>
                 <Grid textAlign='center' verticalAlign='middle' style={{ height: '90vh' }}>
                     <Grid.Column style={{ maxWidth: '90%' }}>
-                        <Grid columns={2}>
-                            <Grid.Column textAlign='center'>
-                                <h2>Agências</h2>
-                            </Grid.Column>
+                        <Grid >
+                            <Grid.Row>
+                                <Container>
+                                <Segment vertical>
+                                    <h1>Agências</h1>
+                                </Segment>
+                                <Segment vertical>
+                                    <h3>Insira seu endereço para ver a agência mais próxima de você.</h3>
+                                </Segment>
+                                </Container>
+                            </Grid.Row>
 
-                            <Grid.Column>
+                            <Grid.Row>
                                 <MapAgencias />
-                            </Grid.Column>
+                            </Grid.Row>
                         </Grid>
 
                     </Grid.Column>
