6d0abb1913d597b9d9087563a00483bf0f7abd37
aluno <joao@gmail.com>
2023-05-16 20:22:15 -0300

    Novo Menu

--- src/components/MenuAdm.jsx
+++ src/components/MenuAdm.jsx
@@ -1,17 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Icon, Image, Menu, Segment } from "semantic-ui-react";
 import logo from '../assets/img/logoGrande.png';
 
-class MenuSistema extends React.Component {
+function MenuSistema ()  {
+   
+    const [activeItem,setActiveItem] = useState();
+    const [estaLogado,setEstaLogado] = useState(true);
 
-    state = {
-        activeItem: 'home',
-    }
-
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-    render() {
         return (
             <>
                 <Segment inverted color="yellow">
@@ -20,25 +16,75 @@ class MenuSistema extends React.Component {
                             <Image src={logo} as={Link} to='/' size='small' />
                         </Menu.Item>
 
+                        {estaLogado &&
+                            <>
+                                <Menu.Item
+                                    name='criarPeticao'
+                                
+                                    
+                                    as={Link}
+                                    to='/criarPeticao'
+                                />
 
-                        <Menu.Item position="right" >
-                            <Button
-                                icon
-                                labelPosition='right'
-                                color='orange'
-                                floated='right'
-                                //onClick={}
-                            >
-                                <Icon name='sign-out' />
-                                Sair
-                            </Button>
-                        </Menu.Item>
-                          
+                                <Menu.Item
+                                    name='meus dados'
+                                  
+                               
+                                    as={Link}
+                                    to='/usuario'
+                                />
+
+                                <Menu.Item
+                                    name='Agencias'
+                           
+                                   
+                                    as={Link}
+                                    to='/agencias'
+                                />
+
+                                <Menu.Item position="right" >
+                                        <Button
+                                            icon
+                                            labelPosition='right'
+                                            color='orange'
+                                            as={Link}
+                                            floated='right'
+                                            to='/login'
+                                        >
+                                            <Icon name='sign-out' />
+                                            Sair
+                                        </Button>
+                                </Menu.Item>
+                            </>
+                        }
+
+
+
+                        {!estaLogado &&
+                            <>
+                                <Menu.Item position="right" >
+                                    
+                                        <Button
+                                            icon
+                                            labelPosition='left'
+                                            color='orange'
+                                            floated='right'
+                                            as={Link}
+                                            to='/login'
+                                            
+                                        >
+                                            <Icon name='file text' />
+                                            Criar Petição
+                                        </Button>
+                                
+                                </Menu.Item>
+                            </>
+                        }
                     </Menu>
                 </Segment>
             </>
         )
-    }
+    
 }
 
 export default MenuSistema;
